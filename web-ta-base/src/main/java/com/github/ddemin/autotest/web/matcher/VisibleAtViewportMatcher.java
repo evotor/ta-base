6c73cda50c37e3f6bf4eee6b62f05118c524d81e
dimand58 <d.demin@evotor.ru>
2017-02-18 17:05:20 +0300

    WEB: Hamcrest matcher for checking that element really displays at current browser viewport

---
+++ web-ta-base/src/main/java/com/github/ddemin/autotest/web/matcher/VisibleAtViewportMatcher.java
@@ -0,0 +1,48 @@
+package com.github.ddemin.autotest.web.matcher;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static java.lang.Math.toIntExact;
+import static org.hamcrest.core.Is.is;
+
+import org.hamcrest.*;
+import org.openqa.selenium.*;
+
+public class VisibleAtViewportMatcher extends TypeSafeMatcher<WebElement> {
+
+  @Override
+  public void describeTo(final Description description) {
+    description.appendText("element fully visible at current viewport");
+  }
+
+  @Override
+  public boolean matchesSafely(final WebElement element) {
+    return isVisibleAtViewport(element);
+  }
+
+  public static boolean isVisibleAtViewport(final WebElement element) {
+    if (!element.isDisplayed()) return false;
+
+    java.awt.Rectangle browserRect = new java.awt.Rectangle(
+        toIntExact(executeJavaScript("return window.pageXOffset;")),
+        toIntExact(executeJavaScript("return window.pageYOffset;")),
+        toIntExact(executeJavaScript("return window.innerWidth;")),
+        toIntExact(executeJavaScript("return window.innerHeight;"))
+    );
+
+    Dimension elementDimension = element.getSize();
+    Point elementLocation = element.getLocation();
+    java.awt.Rectangle elementRect = new java.awt.Rectangle(
+        elementLocation.getX(),
+        elementLocation.getY(),
+        elementDimension.getWidth(),
+        elementDimension.getHeight()
+    );
+
+    return browserRect.contains(elementRect);
+  }
+
+  public static VisibleAtViewportMatcher visibleAtViewport() {
+    return new VisibleAtViewportMatcher();
+  }
+}
