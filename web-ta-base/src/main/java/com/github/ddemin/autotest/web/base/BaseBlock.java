e0fa29d2b8d8c1de89cb6c015ee6a8cecb8e577b
dimand58 <d.demin@evotor.ru>
2017-02-18 17:04:15 +0300

    WEB: BaseBlock now are implements WebElement interface

--- web-ta-base/src/main/java/com/github/ddemin/autotest/web/base/BaseBlock.java
+++ web-ta-base/src/main/java/com/github/ddemin/autotest/web/base/BaseBlock.java
@@ -1,11 +1,15 @@
 package com.github.ddemin.autotest.web.base;
 
+import com.github.ddemin.autotest.web.matcher.*;
+
+import java.util.*;
+
 import com.codeborne.selenide.*;
 import lombok.*;
 import org.openqa.selenium.*;
 
 @AllArgsConstructor
-public abstract class BaseBlock implements IWebBrowserArea {
+public abstract class BaseBlock implements IWebBrowserArea, WebElement {
 
   private SelenideElement root;
 
@@ -34,7 +38,94 @@ public abstract class BaseBlock implements IWebBrowserArea {
     return this;
   }
 
+  public boolean isVisibleAtViewport() {
+    return VisibleAtViewportMatcher.isVisibleAtViewport(getRoot())
+        && getKeyElements().stream().allMatch(el -> VisibleAtViewportMatcher.isVisibleAtViewport(el));
+  }
+
+  @Override
   public void click() {
     getRoot().click();
+  }
+
+  @Override
+  public void submit() {
+    getRoot().submit();
+  }
+
+  @Override
+  public void sendKeys(CharSequence... keysToSend) {
+    getRoot().sendKeys(keysToSend);
+  }
+
+  @Override
+  public void clear() {
+    getRoot().clear();
+  }
+
+  @Override
+  public String getTagName() {
+    return getRoot().getTagName();
+  }
+
+  @Override
+  public String getAttribute(String name) {
+    return getRoot().getAttribute(name);
+  }
+
+  @Override
+  public boolean isDisplayed() {
+    return getRoot().isDisplayed()
+        && getKeyElements().stream().allMatch(WebElement::isDisplayed);
+  }
+
+  @Override
+  public boolean isSelected() {
+    return getRoot().isSelected();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return getRoot().isEnabled();
+  }
+
+  @Override
+  public String getText() {
+    return getRoot().getText();
+  }
+
+  @Override
+  public List<WebElement> findElements(By by) {
+    return getRoot().findElements(by);
+  }
+
+  @Override
+  public WebElement findElement(By by) {
+    return getRoot().findElement(by);
+  }
+
+  @Override
+  public Point getLocation() {
+    return getRoot().getLocation();
+  }
+
+  @Override
+  public Dimension getSize() {
+    return getRoot().getSize();
+  }
+
+  @Override
+  public Rectangle getRect() {
+    return getRoot().getRect();
+  }
+
+  @Override
+  public String getCssValue(String propertyName) {
+    return getRoot().getCssValue(propertyName);
+  }
+
+  @Override
+  public <X> X getScreenshotAs(OutputType<X> target) throws WebDriverException {
+    return getRoot().getScreenshotAs(target);
   }
 }
